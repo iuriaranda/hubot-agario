9eeb878cd0a5ac980bce2ea6077df43d53e618ba
iuri aranda <iuri.aranda@gmail.com>
2015-08-02 11:44:51 +0200

    Correctly parse Agar.io response

--- src/agario.js
+++ src/agario.js
@@ -6,15 +6,21 @@
 //
 
 
+var errMsg = 'There was an error, please try again later';
 module.exports = function(robot) {
   robot.respond(/create agario party/i, function(msg) {
     robot.http('http://m.agar.io/findServer')
     .post('EU-London:party\n154669603')(function(err, res, body) {
       if (err) {
-        msg.reply('There was an error, please try again later');
+        msg.reply(errMsg);
         robot.logger.error('Error finding an Agar.io server: ' + err);
       } else {
-        msg.reply('http://agar.io/#' + body.token);
+        try {
+          msg.reply('http://agar.io/#' + JSON.parse(body).token);
+        } catch (e) {
+          msg.reply(errMsg);
+          robot.logger.error('Error parsing agar.io response: ' + body);
+        }
       }
     });
   });
